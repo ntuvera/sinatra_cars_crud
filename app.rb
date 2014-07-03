467e55904476b3d30ff8b8bf4b3b3eed1d45aa2a
ntuvera <ntuvera@gmail.com>
2014-07-03 13:22:46 -0400

    finish routes in app.rb. work on views

--- app.rb
+++ app.rb
@@ -5,13 +5,46 @@ require_relative 'models/car'
 require_relative './config'
 
 #index
+get '/cars/' do
+  @cars = Car.all
+  erb :index
+end
 
 #new
+get '/cars/new' do
+  erb :new
+end
 
 #show
+get '/cars/:id' do
+  @car = Car.find(params[:id])
+  erb :show
+end
 
 #create
+post '/cars' do
+  car = Car.create(make: params['make'], model: params['model'], year: params['year'], type: params['type'])
+  # car.save
+  redirect "/cars/#{ car.id }"
+end
 
 #edit
+get '/cars/:id' do
+  @car = Car.find(params[:id])
+  erb :edit
+end
+
+put '/cards:/id' do
+  car = Car.find(params[:id])
+  car.make = params[:make]
+  car.model= params[:model]
+  car.year = params[:year]
+  car.type = params[:type]
+  redirect '/cars/#{ car.id }'
+end
 
 #destroy
+delete '/cars/:id' do
+  Car.delete(params[:id])
+  redirect "/cars"
+end
